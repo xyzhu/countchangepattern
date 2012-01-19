f542274f61b59a1697401070900b7912f7ebdab5
xiaoyan <xyzhu@Xiaoyan-Zhus-MacBook.local>
2012-01-19 09:51:11 +0800

    Count the time a statement change

--- patch.py
+++ patch.py
@@ -18,7 +18,9 @@ def extract_patch(cursor,db,start_time,end_time,repo_id,buggy,file_path):
     db.execute_statement_with_param(query, (start_time, end_time, repo_id, "%.java"), cursor)
     patches = cursor.fetchall()
     pattern = [ [0 for i in range(0,18)] for j in range(0,18)]
-    result = [0 for i in range(0,18)]
+    #if n(n>1) of s statement changed in a patch, result_num should add n while result_time should add 1
+    result_num = [0 for i in range(0,18)]
+    result_time = [0 for i in range(0,18)]
     r = [0 for i in range(0,18)]
     num_hunk = 0
     for i in range(0, len(patches)):
@@ -26,20 +28,27 @@ def extract_patch(cursor,db,start_time,end_time,repo_id,buggy,file_path):
         num_hunk = split_patch_to_hunk(patch,file_path)
         r = count_statement(file_path,num_hunk)
         for j in range(0,18):
-            result[j] += r[j]
+            if r[j]!=0:
+                    result_num[j] += r[j]
+                    result_time[j] += 1
         pattern = mine_pattern(r, pattern)
-    save_result(result,file_path,repo_id,num_hunk,pattern,start_time)
+    save_result(result_num,result_time,file_path,repo_id,num_hunk,pattern,start_time)
 
-def save_result(result,file_path,repo_id,num_hunk,pattern,start_time):
-    s = start_time+","
+def save_result(result_num,result_time,file_path,repo_id,num_hunk,pattern,start_time):
+    s_num = start_time+","
+    s_time = start_time+","
     p = ""
     for i in range(0,18):
-        s += str(result[i])+","
+        s_num += str(result_num[i])+","
+        s_time += str(result_time[i])+","
         for j in range(0,18):
             p += str(pattern[i][j])+","
         p += "\n"
-    f = open(file_path+"project_"+repo_id+".csv",'a',)
-    f.write(s+"\n")
+    f = open(file_path+"project_"+repo_id+"_num.csv",'a',)
+    f.write(s_num+"\n")
+    f.close
+    f = open(file_path+"project_"+repo_id+"_time.csv",'a',)
+    f.write(s_time+"\n")
     f.close
     f = open(file_path+"project_"+repo_id+"_pattern_"+start_time+".csv",'a')
     f.write(p)
